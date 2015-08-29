8432af32989238c75eaa73a7347f61bef165c799
yoshii.hisataka <yoshii.hisataka@gmail.com>
2015-08-29 23:55:27 +0900

    accept control allows

--- src/nowhot/http.clj
+++ src/nowhot/http.clj
@@ -4,7 +4,7 @@
 (defn res-json [body]
   {:status 200 :headers {"Content-Type" "application/json; charset=UTF-8"
                          "Access-Control-Allow-Origin" "*"
-                         ”Access-Control-Allow-Headersa" "x-requested-with, content-type, origin, accept"} :body body})
+                         "Access-Control-Allow-Headersa" "x-requested-with, content-type, origin, accept"} :body body})
 ; httpレスポンスを返す
 (defn res-http [body]
   {:status 200 :headers {"Content-Type" "text/html; charset=UTF-8"} :body body})
